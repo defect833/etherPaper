55a12dd6b33cfbf0f0b7437ac9dc4afde418e07f
defect833 <4488690+defect833@users.noreply.github.com>
2018-04-12 17:14:17 +0930

    adding PaperWallet class for containing all details to be written to the output file

---
+++ EtherPaper/PaperWallet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EtherPaper
+{
+    class PaperWallet
+    {
+        public string PrivateKey { get; set; }
+
+        public string Address { get; set; }
+    }
+}
